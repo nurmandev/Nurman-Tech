4b0cd064da2f6b955e4c9bae009d0bded83e2a69
Adebayour66265 <adebayour66265@gmail.com>
2024-02-06 08:58:21 +0100

    add footer

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,9 +3,7 @@ import FirstSection from "./_components/first-section";
 import SecondSection from "./_components/second-section";
 import ThirdSection from "./_components/third-section";
 import FeedbackSection from "./_components/feedback-section";
-import Slider from "../learning/_components/slider";
 
-import Testimonials from "../bird-ai/_components/testimonials";
 import FourthSection from "./_components/fourth-section";
 import FAQS from "./_components/faqs";
 import PhoneSection from "./_components/phone-section";
@@ -13,22 +11,20 @@ import BottomSection from "@/components/ui/bottom-section";
 import Footer from "@/components/ui/footer";
 
 const ScreenRecorder = () => {
-    return ( 
+  return (
     <div>
-        <Navbar />
-        <FirstSection />
-        <SecondSection />
-        <ThirdSection />
-        <FeedbackSection />
-        <Slider />
-        <Testimonials />
-        <FourthSection />
-        <FAQS />
-        <PhoneSection />
-        <BottomSection />
-        <Footer />
- 
-    </div> );
-}
- 
+      <Navbar />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FeedbackSection />
+      <FourthSection />
+      <FAQS />
+      <PhoneSection />
+      <BottomSection />
+      <Footer />
+    </div>
+  );
+};
+
 export default ScreenRecorder;
