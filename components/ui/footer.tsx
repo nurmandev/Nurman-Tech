39249fb0bcfa07f2ec886b3efee3c993467b6bd6
Adebayour66265 <adebayour66265@gmail.com>
2024-02-06 08:43:15 +0100

    add footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,48 +1,47 @@
-import { Facebook, Globe, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+import {
+  Facebook,
+  Globe,
+  Instagram,
+  Linkedin,
+  Twitter,
+  Youtube,
+} from "lucide-react";
+import Link from "next/link";
 
 const Footer = () => {
-    return ( 
-    
+  return (
     <div>
-        <section className="md:flex flex-col md:py-10 md:justify-between">
-            <div className="p-10 space-y-10 md:space-y-0 md:text-center md:flex justify-between md:px-80 md:space-x-20">
-                <div>
-                ©2024 Nurman Innovation. All rights reserved.
-                </div>
-                <div>
-                    <Globe className="h-16 w-16" />
-                </div>
-                <div>
-                    Terms and Conditions
-                </div>
+      <section className="md:flex flex-col md:py-10 md:justify-between">
+        <div className="p-10 space-y-10 md:space-y-0 md:text-center md:flex justify-between md:px-80 md:space-x-20">
+          <div>©2024 Nurman Innovation. All rights reserved.</div>
+          <div>
+            <Globe className="h-16 w-16" />
+          </div>
+          <div>Terms and Conditions</div>
 
-                <div className="flex items-center justify-center space-x-2">
-                    <div className="bg-black p-2 rounded-full">
-                        <Twitter className="h-6 w-6 text-white" />
-                    </div>
-                    <div className="bg-black p-2 rounded-full">
-                        <Facebook className="h-6 w-6 text-white" />
-                    </div>
-
-                    <div className="bg-black p-2 rounded-full">
-                        <Instagram className="h-6 w-6 text-white" />
-                    </div>
-
-                    <div className="bg-black p-2 rounded-full">
-                        <Linkedin className="h-6 w-6 text-white" />
-                    </div>
-
-                    <div className="bg-black p-2 rounded-full">
-                        <Youtube className="h-6 w-6 text-white" />
-                    </div>
-
-                </div>
-
+          <div className="flex items-center justify-center space-x-2">
+            <div className="bg-black p-2 rounded-full">
+              <Link href="https://web.facebook.com/proficientng/">
+                <Facebook className="h-6 w-6 text-white" />
+              </Link>
             </div>
 
-        </section>
+            <div className="bg-black p-2 rounded-full">
+              <Link href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Fnurman_ng%3Figsh%3Ddjk5d2o2endneTY2%26fbclid%3DIwAR1f4pctlQmDpeZWVtPp2HXjDoCTMHeV5v0q8P_S7vDEpDr1G836a3-ci8E&h=AT00qRKR0TPsZLHkG8hPX3hykv-Fhqm7jWZ0OM6wVlIvV7vPXTZD_jT4-vAD6Tzlh4dj-dF2sFz1WekXkmXunY_iCsRuUFafIGa22AQ8eavtIyzJekCrgiuIaN-HdIEIwUyy3w">
+                <Instagram className="h-6 w-6 text-white" />
+              </Link>
+            </div>
 
-    </div> );
-}
- 
+            <div className="bg-black p-2 rounded-full">
+              <Link href="https://www.linkedin.com/company/nurman-tech">
+                <Linkedin className="h-6 w-6 text-white" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
 export default Footer;
